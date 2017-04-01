0d4d7de166c0113f89d68d398e84bcc5761f9d8d
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-01 20:53:08 +0300

    SUM -> result

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main ()
 	setlocale(LC_ALL,"RUS");
 	size_t N;
 	double x;
-	double SUM;
+	double result;
 
 	std::cout<<"Введите размерность N полинома a:"<<std::endl;
 	std::cin>>N; //задаем размерность полинома
@@ -27,7 +27,7 @@ int main ()
 	Polynom b(N); //создаем объект класса Полином
 	for (size_t i= 0; i<N; i++) //заполняем пустой массив коэффициентов
 	{
-		std::cout<<"Введите коэффициент A"<<i<<std::endl;
+		std::cout<<"Введите коэффициент B"<<i<<std::endl;
 		std::cin>>b[i];
 	}
 
@@ -37,16 +37,16 @@ int main ()
 	Polynom c = Polynom(N); //создаем объект класса Полином
 	for (size_t i= 0; i<N; i++) //заполняем пустой массив коэффициентов
 	{
-		std::cout<<"Введите коэффициент A"<<i<<std::endl;
+		std::cout<<"Введите коэффициент C"<<i<<std::endl;
 		std::cin>>c[i];
 	}
 
-	/*std::cout<<"Введите значение переменной X"<<std::endl;
-	std::cin>>x; //вводим значение переменной X
+	std::cout<<"Введите значение переменной x"<<std::endl;
+	std::cin>>x; //вводим значение переменной x
 
-	SUM=a(x);
+	result=a(x);
 
-	std::cout<<"Сумма полинома: "<<SUM<<std::endl; //вывод значения суммы*/
+	std::cout<<"Сумма полинома: "<<result<<std::endl; //вывод значения суммы
 
 	system("pause");
 	return 0;
