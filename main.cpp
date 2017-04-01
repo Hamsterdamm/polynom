2424a80519ace14b9ea836eaba53c47c1b142396
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-01 21:59:01 +0300

    test code for main

--- main.cpp
+++ main.cpp
@@ -21,6 +21,14 @@ int main ()
 		std::cin>>a[i];
 	}
 
+	std::cout <<"Полином a:"<< std::endl;
+	for (size_t i = 0; i<a.GetPow(); i++) //
+	{
+		std::cout << a[i] << "*x^" << i << '+';
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "Введите размерность N полинома b:" << std::endl;
 	std::cin >> N; //задаем размерность полинома
 
@@ -31,15 +39,48 @@ int main ()
 		std::cin>>b[i];
 	}
 
-	std::cout << "Введите размерность N полинома c:" << std::endl;
-	std::cin >> N; //задаем размерность полинома
-
-	Polynom c = Polynom(N); //создаем объект класса Полином
-	for (size_t i= 0; i<N; i++) //заполняем пустой массив коэффициентов
+	std::cout << "Полином b:" << std::endl;
+	for (size_t i = 0; i<b.GetPow(); i++) //
 	{
-		std::cout<<"Введите коэффициент C"<<i<<std::endl;
-		std::cin>>c[i];
+		std::cout << b[i] << "*x^" << i << '+';
 	}
+	std::cout << std::endl;
+
+	Polynom sum( a + b);
+
+	std::cout << "Полином a+b:" << std::endl;
+	for (size_t i = 0; i<sum.GetPow(); i++) //
+	{
+		std::cout << sum[i] << "*x^" << i << '+';
+	}
+	std::cout << std::endl;
+
+	Polynom dif = a - b;
+
+	std::cout << "Полином a-b:" << std::endl;
+	for (size_t i = 0; i<dif.GetPow(); i++) //
+	{
+		std::cout << dif[i] << "*x^" << i << '+';
+	}
+	std::cout << std::endl;
+
+	Polynom mul = a * b;
+
+	std::cout << "Полином a*b:" << std::endl;
+	for (size_t i = 0; i<mul.GetPow(); i++) //
+	{
+		std::cout << mul[i] << "*x^" << i << '+';
+	}
+	std::cout << std::endl;
+
+	Polynom der = a.diff();
+
+	std::cout << "Полином a':" << std::endl;
+	for (size_t i = 0; i<der.GetPow(); i++) //
+	{
+		std::cout << der[i] << "*x^" << i << '+';
+	}
+	std::cout << std::endl;
 
 	std::cout<<"Введите значение переменной x"<<std::endl;
 	std::cin>>x; //вводим значение переменной x
